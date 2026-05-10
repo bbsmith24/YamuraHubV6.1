7d0bc6025d477d9ff88aac2c01c1258e7bd0785a
bbsmith24 <bbsmith024@gmail.com>
2026-05-10 09:03:06 -0400

    Added header
    Added header in main.cpp. Initial checking of locally implemented FTPClient, Teensy 4.0 + Airlift + ESP32 TFT display with built in microSD card. Might be possible to add Airlift functionality to the TFT display....
    
    Broke TFT menu system out into it's own (at this point partially complete...) class

--- src/main.cpp
+++ src/main.cpp
@@ -1,5 +1,45 @@
-#define DEBUG_VERBOSE
-#define DEBUG_EXTRA_VERBOSE
+//
+// YamuraLog Hub
+// Teensy 4.0/CAN FD
+// Brian B. Smith
+// Yamura Motors LLC
+// brianbsmith.com
+// February 2022
+//
+// 05/2026 
+// replaced open source FTP client with custom implementation to 
+// support large file transfers and directory listing. Also eliminated
+// versioning issues with Arduino libraries by implementing FTP client 
+// functionality directly in the main codebase. Uses ESP FTP display board
+// with built in microSD card slot for WiFi and file storage
+// 
+// Requires FlexCAN_T4 for Teensy 4.0/4.1 CAN 
+// can1 and can2 are CAN2.0B
+// can3 is CAN FD
+//
+//  MIT License
+//  Copyright (c) 2022 Brian B Smith - info@brianbsmith.com
+//
+//  Permission is hereby granted, free of charge, to any person obtaining a copy
+//  of this software and associated documentation files (the "Software"), to deal
+//  in the Software without restriction, including without limitation the rights
+//  to use, copy, modify, merge, publish, distribute, sublicense, and / or sell
+//  copies of the Software, and to permit persons to whom the Software is
+//  furnished to do so, subject to the following conditions:
+//
+//  The above copyright notice and this permission notice shall be included in all
+//  copies or substantial portions of the Software.
+//
+//  THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+//  IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+//  FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+//  AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+//  LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+//  OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+//  SOFTWARE.
+// 
+//#define DEBUG_VERBOSE
+//#define DEBUG_EXTRA_VERBOSE
 
 #include "main.h"        // Contains pin definitions
 
